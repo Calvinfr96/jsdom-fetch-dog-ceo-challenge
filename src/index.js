33ba92620dc870bbefd0b2ccc7d7505fe3fded34
calvin <calvin.francis96@outlook.com>
2021-04-11 09:24:02 -0400

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 console.log('%c HI', 'color: firebrick')
+
+//Fetching dog images (challenge 1)
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch(imgUrl)
+    .then(response => response.json())
+    .then(dogs => dogs.message.forEach(dog => {
+        appendImage(dog)
+    }));
+
+function appendImage(image) {
+    const dogImageContainer = document.getElementById("dog-image-container");
+    const img = document.createElement('img');
+    img.src = image
+    img.width = "300"
+    dogImageContainer.appendChild(img)
+}
+
+//Fetching dog breeds (challenge 2)
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+fetch(breedUrl)
+    .then(response => response.json())
+    .then(breeds => appendBreeds(getBreeds(breeds.message)))
+
+function getBreeds(breeds) {
+    breedsList = [];
+    for (const property in breeds) {
+        breedsList.push(property)
+    }
+
+    return breedsList
+}
+function appendBreeds(breedsList) {
+    const ul = document.getElementById("dog-breeds");
+    breedsList.forEach(breed => {
+        const li = document.createElement('li');
+        li.innerText = breed
+        li.addEventListener('click', function () { changeListColor(li) })
+        ul.appendChild(li)
+    })
+}
+
+//Changing font color on click (Challenge 3)
+function changeListColor(listItem) {
+    listItem.style.color = 'red';
+}
